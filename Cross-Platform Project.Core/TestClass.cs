a40835c89f3c7899081440b8dc255d24aa30e206
Tom Kaczocha <tom@crazydev.org>
2012-12-11 10:36:07 +1100

    Initial Project Commit

---
+++ Cross-Platform Project.Core/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cross_Platform_Project.Core
+{
+    class TestClass
+    {
+    }
+}
